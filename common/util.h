78052c281fbba09f80d0c50d08681376690a4e47
lotuskip <onnir@iki.fi>
2011-02-27 14:31:16 +0200

    added nontracked manual image file...

--- common/util.h
+++ common/util.h
@@ -3,7 +3,7 @@
 #define UTIL_H
 
 // When we want a random number between 0 and N-1, we usually do
-// random()%N. But if N is equal to 0 we want to get 0, and for that
+// random()%N. But if N is equal to 1 we want to get 0, and for that
 // purpose use this:
 short randor0(const short size);
 
